db41ce083fcdb7615f3490b62c7d4eb74d91d7de
Sebastian Krauss <sebastian.krauss@bridging-it.de>
2021-02-22 21:29:29 +0100

    raumnamen

--- ET_Raumnamen.tsx
+++ ET_Raumnamen.tsx
@@ -2,18 +2,18 @@
 <tileset version="1.4" tiledversion="1.4.3" name="ET_Raumnamen" tilewidth="164" tileheight="32" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="6">
-  <image width="164" height="32" source="../../../Downloads/text2image_X6368896_20210222_193031.png"/>
+  <image width="164" height="32" source="tiles_png/ET_Raumnamen/text2image_X6368896_20210222_193031.png"/>
  </tile>
  <tile id="7">
-  <image width="164" height="32" source="../../../Downloads/text2image_L9258265_20210222_193127.png"/>
+  <image width="164" height="32" source="tiles_png/ET_Raumnamen/text2image_L9258265_20210222_193127.png"/>
  </tile>
  <tile id="8">
-  <image width="164" height="32" source="../../../Downloads/text2image_J2274180_20210222_193433.png"/>
+  <image width="164" height="32" source="tiles_png/ET_Raumnamen/text2image_J2274180_20210222_193433.png"/>
  </tile>
  <tile id="9">
-  <image width="164" height="32" source="../../../Downloads/text2image_F6618180_20210222_193529.png"/>
+  <image width="164" height="32" source="tiles_png/ET_Raumnamen/text2image_F6618180_20210222_193529.png"/>
  </tile>
  <tile id="10">
-  <image width="164" height="32" source="../../../Downloads/text2image_P1840404_20210222_193614.png"/>
+  <image width="164" height="32" source="tiles_png/ET_Raumnamen/text2image_P1840404_20210222_193614.png"/>
  </tile>
 </tileset>
