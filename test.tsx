4375f40c076ad04705c898e4f596456a9bdbb766
Sebastian Krauss <sebastian.krauss@bridging-it.de>
2021-02-22 21:17:39 +0100

    neue maps

---
+++ test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="test" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="../../../Downloads/text2image_P9195278_20210222_191937.png" width="200" height="150"/>
+</tileset>
---
+++ text.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="text" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <grid orientation="orthogonal" width="30" height="30"/>
+ <image source="../../../Downloads/text2image_H7599083_20210222_192252.png" width="200" height="200"/>
+</tileset>
